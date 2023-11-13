b5e221399ae8c54ef3ec049004b1088ea3781dfb
Freddy J Moreno B <freddy.moreno@gmail.com>
2023-11-13 19:50:53 -0300

    Feat: Add exception filter and logger into main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { Logger } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './infrastructure/common/exceptions/http-exception.filter';
 
 async function bootstrap() {
+  const logger = new Logger();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Environment: ${process.env.ENVIRONMENT}`);
+  logger.log(`Server Start Port ${port}`);
 }
-bootstrap();
+
+bootstrap().then(() => {});
